5ae5789cf47215c53ca6321171262bbffb608600
Luis Gonzales <luisgonzacc@gmail.com>
2021-11-16 03:48:54 +0000

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - free a list
+ *@head: pointer points to a pointer to head
+ *
+ *Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+
+if (head == NULL)
+return;
+
+while (*head != NULL)
+{
+tmp = *head;
+*head = tmp->next;
+free(tmp);
+}
+}
